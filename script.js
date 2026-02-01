d3aac51e0da9aa574e59b57b6f04db3d0351b800
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-01 15:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+let squares = document.querySelectorAll(".square");
+
+for (let box of squares) {
+  box.addEventListener("mouseenter", hover);
+  box.addEventListener("mouseleave", reset);
+}
+
+function hover(event) {
+  for (let box of squares) {
+    if (box !== event.target) {
+      box.style.backgroundColor = "#6F4E37"; // Coffee
+    }
+  }
+}
+
+function reset() {
+  for (let box of squares) {
+    box.style.backgroundColor = "#E6E6FA"; // Lavender
+  }
+}
+
+
+
+
+
+
+
+
+
